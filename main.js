7fa805a5b4be585b6995e21b060398a920fcf454
A242 <yorge360@gmail.com>
2016-07-15 11:29:42 -0400

    Update main.js

--- main.js
+++ main.js
@@ -15,12 +15,17 @@ var mainState = {
                     Phaser.Keyboard.SPACEBAR);
         spaceKey.onDown.add(this.jump, this);
         this.pipes = game.add.group();
-        this.timer = game.time.events.loop(1500, this.addRowOfPipes, this); 
+        this.timer = game.time.events.loop(1500, this.addRowOfPipes, this);
+        this.score = 0;
+        this.labelScore = game.add.text(20, 20, "0",
+    { font: "30px Arial", fill: "#ffffff" });
     },
 
   update: function() {
         if (this.bird.y < 0 || this.bird.y > 490){
         this.restartGame();
+        game.physics.arcade.overlap(
+    this.bird, this.pipes, this.restartGame, null, this);
       }
     },
   jump: function() {
@@ -41,8 +46,10 @@ var mainState = {
     addRowOfPipes: function() {
     var hole = Math.floor(Math.random() * 5) + 1;
     for (var i = 0; i < 8; i++)
-        if (i != hole && i != hole + 1)
-            this.addOnePipe(400, i * 60 + 10);
+      if (i != hole && i != hole + 1)
+      this.addOnePipe(400, i * 60 + 10);
+      this.score += 1;
+      this.labelScore.text = this.score;
 },
 };
 
